fd4ce690f62ad50a5184595b445a110fb9f95180
Manuel Alva Barrera <jalvabarrera1@nait.ca>
2024-11-06 11:57:00 -0700

    Setup Project

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+import express from "express";
+import path from "path";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import nodemailer from "nodemailer";
+import { fileURLToPath } from "url";
+import multer from "multer";
+
+dotenv.config();
+const app = express();
+
+// Get the current directory
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Connect to MongoDB Atlas
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected Successfully to MongoDB Atlas"))
+  .catch((error) => console.error("MongoDB connection error:", error));
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Serve static assets in production mode
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(process.cwd(), "frontend", "public")));
+  app.use("/src", express.static(path.join(process.cwd(), "frontend", "src")));
+  app.use(
+    "/components",
+    express.static(path.join(process.cwd(), "frontend", "src", "components"))
+  );
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(process.cwd(), "frontend", "public", "index.html")
+    );
+  });
+}
+
+// Listen on port 5001
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
